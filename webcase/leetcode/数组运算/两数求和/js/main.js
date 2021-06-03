4f723353b284a8227603c767e38cce58de22ad94
richardadda <1156282805@qq.com>
2021-06-04 00:29:32 +0800

    feat: modify - daily commit

---
+++ webcase/leetcode/数组运算/两数求和/js/main.js
@@ -0,0 +1,23 @@
+/*
+    给定一个整数数组 nums 和一个整数目标值 target，请你在该数组中找出 和为目标值 target  的那 两个 整数，并返回它们的数组下标
+    你可以假设每种输入只会对应一个答案。但是，数组中同一个元素在答案里不能重复出现
+ */
+function twoSum (nums, target) {
+    const len = nums.length
+    if (len <= 1) {
+        return []
+    }
+    for (let i = 0; i < len - 1; i++) {
+        for (let j = i + 1; j < len; j++) {
+            if ((nums[i] + nums[j]) == target) {
+                return [i, j]
+            }
+        }
+    }
+    return []
+}
+
+const array = [3, 2, 4]
+const target = 7
+
+console.log(twoSum(array, target))
