4e4f390fc3029b54bde9ba31cbc200b10a3a22c2
richardadda <1156282805@qq.com>
2021-06-10 00:16:52 +0800

    feat: modify - daily commit

---
+++ webcase/algorithm/二叉树/js/main.js
@@ -0,0 +1,10 @@
+/* 二叉树结点 */
+class BinaryTreeNode {
+    constructor(key, value) {
+        this.parent = null
+        this.left = null
+        this.right = null
+        this.key = key
+        this.value = value
+    }
+}
