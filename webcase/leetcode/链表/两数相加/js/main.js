9984bd347f305409f5e88c120603ad3d8e5ffcbe
richardadda <1156282805@qq.com>
2021-08-04 00:20:04 +0800

    feat: modify - daily commit

--- webcase/leetcode/链表/两数相加/js/main.js
+++ webcase/leetcode/链表/两数相加/js/main.js
@@ -6,95 +6,64 @@
     你可以假设除了数字 0 之外，这两个数都不会以 0 开头
  */
 
-class Node {
-    constructor(data) {
-        this.data = data
-        this.prev = null
-        this.next = null
-    }
-}
-
-class NodeList {
-    constructor() {
-        this.size = 0
-        this.head = new Node('head')
-        this.currNode = ''
-    }
-    
-    isEmpty() {
-        return this.size === 0
-    }
-
-    findLast() {
-        let currNode = this.head
-        while (currNode.next) {
-            currNode = currNode.next
-        }
-        return currNode
-    }
-    
-    display() {
-        let result = ''
-        let currNode = this.head
-        while (currNode) {
-            result += currNode.data
-            currNode = currNode.next
-            if(currNode) {
-                result += '->'
-            }
-        }
-        return result
-    }
-
-    find(item) {
-        let currNode = this.head
-        while (currNode && (currNode.data !== item)) {
-            currNode = currNode.next
-        }
-        return currNode
-    }
-
-    getLength() {
-        return this.size
-    }
-
-    insert(item, element) {
-        let itemNode = this.find(item)
-        if(!itemNode) {
-            return
-        }
-        let newNode = new Node(element)
-
-        newNode.next = itemNode.next
-        itemNode.next = newNode        
-        this.size++
-    }
-
-    remove(item) {
-        if(!this.find(item)) {
-            return;
-        }
-        let currNode = this.head
-        while (currNode.next.data !== item) {
-            if (!currNode.next) {
-                return
-            }
-            currNode = currNode.next
-        }
-        currNode.next = currNode.next.next
-        this.size--
-    }
-
-    append(element) {
-        let currNode = this.findLast()
-        let newNode = new Node(element)
-        currNode.next = newNode
-        this.size++
-    }
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+function ListNode(val, next) {
+    this.val = (val === undefined ? 0 : val)
+    this.next = (next === undefined ? null : next)
 }
 
 function addTwoNumbers (list1, list2) {
-    
+    const maxLength = Math.max(list1.length, list2.length)
+    const temp = []
+    if (list1.length < maxLength) {
+        const len = list1.length
+        for (let i = 0; i < maxLength - len; i++) {
+            list1.unshift(0)
+        }
+    }
+    if (list2.length < maxLength) {
+        const len = list2.length
+        for (let i = 0; i < maxLength - len; i++) {
+            list2.unshift(0)
+        }
+    }
+    for (let i = 0; i < maxLength + 1; i++) {
+        temp[i] = 0
+    }
+    for (let i = maxLength - 1, j = i + 1; i >= 0; i--) {
+        const sum = list1[i] + list2[i] + temp[j]
+        if (sum >= 10) {
+            temp[j] = sum - 10
+            temp[j - 1] = 1
+        } else {
+            temp[j] = sum
+        }        
+        j--
+    }
+    let res = []
+    let flag = false
+    for (let i = 0; i < temp.length; i++) {
+        if (temp[i] <= 0 && !flag) {
+            continue
+        } else {
+            flag = true
+        }
+        res.unshift(temp[i])
+    }
+    const head = new ListNode(res[0], null)
+    let p = head
+    for (let i = 1; i < res.length; i++) {
+        const node = new ListNode(res[i], null)
+        p.next = node
+        p = node
+    }
+    return head
 }
 
 const list1 = [2, 4, 3]
