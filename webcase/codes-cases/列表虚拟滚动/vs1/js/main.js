9c7a24995d6130590d3c7421030903ece6351094
ctime-zliang <1156282805@qq.com>
2021-09-26 17:40:48 +0800

    feat: modify - add Virtual Scroller Class v1

---
+++ webcase/codes-cases/列表虚拟滚动/vs1/js/main.js
@@ -0,0 +1,18 @@
+﻿const MAX_LENGTH = 1000
+function dataCreator() {
+    const list = []
+    for (let i = 0; i < MAX_LENGTH; i++) {
+        list.push({id: i + 1, content: `The Content ${i+1}`})
+    }
+    return list
+}
+
+
+window.onload = function() {
+    const outerContainer = document.querySelector('[data-tagitem="virtual-scroller"]')
+    const virtualScroller = new VirtualScroller(outerContainer, {
+        rowsViewCount: 10
+    })    
+    virtualScroller.setData(dataCreator())
+    console.log(virtualScroller)    
+}
---
+++ webcase/codes-cases/列表虚拟滚动/vs1/js/virtual-scroller.js
@@ -0,0 +1,103 @@
+﻿const positionContainer = (htmlString = '') => {
+    return `<div style="z-index: -9999; position: absolute; left: -9999px; top: -9999px;">${htmlString}</div>`
+}
+const virtualScrollerFragmentTemplate = () => {
+    return `
+        <div class="virtual-scroller-container">
+            <div class="virtual-scroller-wrapper">
+                <div class="virtual-scroller-content">
+                    <div class="virtual-scroller-ulist">
+                        <div class="virtual-scroller-item"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    `
+}
+const virtualScrollerItemTemplate = (itemData = {}) => {
+    return `<div data-itemid="${itemData.id}" class="virtual-scroller-item">${itemData.content}</div>`
+}
+const defaultRuntimeConfig = {
+    lastIndex: 0,
+    scrollTop: 0,
+    rowItemHeight: 30,
+    rowsViewCount: 20,
+    rowsAllCount: 0,
+    rowsData: []
+}
+
+class VirtualScroller {
+    constructor(el, option = {}) {
+        this.RuntimeConfig = { ...defaultRuntimeConfig, ...option }
+        this.RuntimeConfig.outerContainer = el
+        this._init()
+        this._initFragment()
+        this._bindEvent()
+    }
+
+    setData(list) {
+        const RuntimeConfig = this.RuntimeConfig
+        RuntimeConfig.rowsData = list
+        RuntimeConfig.rowsAllCount = list.length
+        this._setContentRect()
+        this._insertHtml()
+    }
+
+    _init() {
+        const RuntimeConfig = this.RuntimeConfig
+        RuntimeConfig.outerContainerRect = RuntimeConfig.outerContainer.getBoundingClientRect()        
+    }
+
+    _initFragment() {
+        const RuntimeConfig = this.RuntimeConfig
+        document.body.appendChild(document.createRange().createContextualFragment(positionContainer(virtualScrollerFragmentTemplate())))
+        const positionElement = document.body.lastElementChild
+        const ulistElement = positionElement.querySelector(`.virtual-scroller-ulist`)
+        const itemElement = ulistElement.firstElementChild
+        /* ... */
+        ulistElement.style.maxHeight = RuntimeConfig.outerContainerRect.height + 'px'
+        RuntimeConfig.rowItemHeight = itemElement.getBoundingClientRect().height
+        ulistElement.innerHTML = ''
+        RuntimeConfig.outerContainer.innerHTML = ''
+        RuntimeConfig.outerContainer.appendChild(positionElement.firstElementChild)        
+        document.body.removeChild(positionElement)
+        RuntimeConfig.container = RuntimeConfig.outerContainer.firstElementChild
+    }
+
+    _setContentRect() {
+        const RuntimeConfig = this.RuntimeConfig
+        const contentElement = RuntimeConfig.container.querySelector(`.virtual-scroller-content`)
+        contentElement.style.height = `${RuntimeConfig.rowsAllCount * RuntimeConfig.rowItemHeight}px`
+        contentElement.style.paddingTop = `${RuntimeConfig.scrollTop}px`
+    }
+
+    _bindEvent() {
+        const self = this
+        const RuntimeConfig = this.RuntimeConfig
+        const wrapperElement = RuntimeConfig.container.querySelector(`.virtual-scroller-wrapper`)
+        const contentElement = RuntimeConfig.container.querySelector(`.virtual-scroller-content`)
+        wrapperElement.addEventListener('scroll', function(evte) {
+            const eventScrollTop = evte.currentTarget.scrollTop
+            if (eventScrollTop > RuntimeConfig.scrollTop && RuntimeConfig.lastIndex + RuntimeConfig.rowsViewCount > RuntimeConfig.rowsAllCount) {
+                return
+            }
+            RuntimeConfig.scrollTop = eventScrollTop
+            RuntimeConfig.lastIndex = ~~((RuntimeConfig.scrollTop / RuntimeConfig.rowItemHeight) | 0)
+            contentElement.style.paddingTop = `${RuntimeConfig.scrollTop}px`
+            self._insertHtml()
+        })
+    }
+
+    _insertHtml() {
+        const RuntimeConfig = this.RuntimeConfig
+        const ulistElement = RuntimeConfig.container.querySelector(`.virtual-scroller-ulist`)
+        const sliceData = RuntimeConfig.rowsData.slice(RuntimeConfig.lastIndex, RuntimeConfig.lastIndex + RuntimeConfig.rowsViewCount)
+        const htmlString = 
+            sliceData
+            .map((item, index) => {
+                return virtualScrollerItemTemplate(item)
+            })
+            .join('\n')
+        ulistElement.innerHTML = htmlString
+    }
+}
