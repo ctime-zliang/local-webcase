b2e83a881ebd9d94d58e04bedb6b14bfef75dd5b
ctime-zliang <1156282805@qq.com>
2021-09-28 16:48:47 +0800

    feat: modify - update transform-style, not paddingTop-style

--- webcase/codes-cases/func-list_virtual_scroll/vs1/js/virtual-scroller.js
+++ webcase/codes-cases/func-list_virtual_scroll/vs1/js/virtual-scroller.js
@@ -21,7 +21,11 @@ const defaultConfig = {
     /* 滚动过程中渲染在可视区域的第一个元素数据在总列表中的索引 */
     viewStartIndex: 0,
     /* scroller-content 元素的内顶边距 */
-    contentAreaPaddingTop: 0,
+    contentAreaYOffset: 0,
+    /* scroller-content 元素的内顶边距可设置的最小值 */
+    contentAreaYOffsetMin: 0,
+    /* scroller-content 元素的内顶边距可设置的最大值 */
+    contentAreaYOffsetMax: 0,
     /* 单行元素的高度 */
     rowItemHeight: 30,
     /* 列表总高度 */
@@ -58,6 +62,7 @@ class VirtualScroller {
         RuntimeConfig.listData = list
         RuntimeConfig.dataAllCount = list.length
         RuntimeConfig.rowsAllHeight = RuntimeConfig.dataAllCount * RuntimeConfig.rowItemHeight
+        RuntimeConfig.contentAreaYOffsetMax = RuntimeConfig.rowsAllHeight - RuntimeConfig.outerContainerRect.height
         this._setContentRect()
         this._insertHtml(this._sliceListData())
     }
@@ -97,32 +102,33 @@ class VirtualScroller {
         const RuntimeConfig = this.RuntimeConfig
         const contentElement = RuntimeConfig.container.querySelector(`.virtual-scroller-content`)
         contentElement.style.height = `${RuntimeConfig.dataAllCount * RuntimeConfig.rowItemHeight}px`
-        contentElement.style.paddingTop = `${RuntimeConfig.contentAreaPaddingTop}px`
+        contentElement.style.paddingTop = `${RuntimeConfig.contentAreaYOffset}px`
     }
 
     _bindEvent() {
         const self = this
         const RuntimeConfig = this.RuntimeConfig
         const wrapperElement = RuntimeConfig.container.querySelector(`.virtual-scroller-wrapper`)
-        const contentElement = RuntimeConfig.container.querySelector(`.virtual-scroller-content`)
-        const scrollMin = 0        
+        const contentElement = RuntimeConfig.container.querySelector(`.virtual-scroller-content`)   
+        const ulistElement = RuntimeConfig.container.querySelector(`.virtual-scroller-ulist`)
         wrapperElement.addEventListener('scroll', function(evte) {
             let scrollTop = evte.currentTarget.scrollTop
-            let scrollMax = RuntimeConfig.rowsAllHeight - RuntimeConfig.outerContainerRect.height
+            console.log(scrollTop)
             /* 阈值判断 */
-            if (scrollTop <= scrollMin) {
+            if (scrollTop <= RuntimeConfig.contentAreaYOffsetMin) {
                 self._emit(EVENT_NAME_CONFIG.SCROLL_TO_TOP)
-                scrollTop = scrollMin
+                scrollTop = RuntimeConfig.contentAreaYOffsetMin
             }
-            if (scrollTop >= scrollMax) {
+            if (scrollTop >= RuntimeConfig.contentAreaYOffsetMax) {
                 self._emit(EVENT_NAME_CONFIG.SCROLL_TO_BOTTOM)               
-                scrollTop = scrollMax
+                scrollTop = RuntimeConfig.contentAreaYOffsetMax
             }
             /* 修改视图 */
-            RuntimeConfig.contentAreaPaddingTop = scrollTop
-            contentElement.style.paddingTop = `${RuntimeConfig.contentAreaPaddingTop}px`
+            RuntimeConfig.contentAreaYOffset = scrollTop
+            // contentElement.style.paddingTop = `${RuntimeConfig.contentAreaYOffset}px`
+            ulistElement.style.transform = `translate3d(0, ${RuntimeConfig.contentAreaYOffset}px, 5px)`
             /* 计算截取开始位所以你 */
-            RuntimeConfig.viewStartIndex = Math.floor((RuntimeConfig.contentAreaPaddingTop / RuntimeConfig.rowItemHeight)) || 0
+            RuntimeConfig.viewStartIndex = Math.floor((RuntimeConfig.contentAreaYOffset / RuntimeConfig.rowItemHeight)) || 0
             /* 渲染列表 */       
             self._insertHtml(self._sliceListData())
         })
