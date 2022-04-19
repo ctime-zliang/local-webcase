7e29402e3947f35b114d4f1732cbd74c2213eead
ctime-zliang <1156282805@qq.com>
2022-04-19 11:38:53 +0800

    feat: modify - oo-base about

---
+++ webcase/codes-cases/oo-base/js/inherit.js
@@ -0,0 +1,14 @@
+﻿function Biological(type) {
+    this.type = type
+    /* 引用类型的属性 */
+    this.features = [`move`]
+    /* 本地方法 */
+    this.talk = function() {
+        return `I can talk.`
+    }
+}
+Biological.prototype.phyComposition = [`heart*1`]
+Biological.prototype.ptt = `Biological PTT`
+Biological.prototype.eat = function() {
+    return `A(n) ${this.type} his name is ${this.name || '-'} is eating.`
+}
