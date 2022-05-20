701b71934cc4719fce7ba484b205c3e05d4729c4
ctime-zliang <1156282805@qq.com>
2022-05-20 15:31:11 +0800

    feat: modify - add deepClone function

--- $vendor/utils/deepClone.js
+++ $vendor/utils/deepClone.js
@@ -1,32 +1,68 @@
 /**
  * @description JSON 深拷贝
  * @function ven$deepClone
- * @param {object|array} json 被深拷贝的 JSON 数据
+ * @param {object|array} data 被深拷贝的 JSON 数据
  * @return {object|array}
  */
-function ven$deepClone(json) {
+function ven$deepClone(data) {
     let wMap = new WeakMap()
-    return traverse(json)
+    return traverse(data)
 
-    function traverse(obj){
+    function traverse(data){
         const result = {}
         let keys = []
         let item = null
 
-        if (wMap.get(obj)) {
-            return wMap.get(obj)
+        if (wMap.get(data)) {
+            return wMap.get(data)
         }
-        keys = Object.keys(obj)
-        wMap.set(obj, result)
+        keys = Object.keys(data)
+        wMap.set(data, result)
 
         for (let i = 0; i < keys.length; i++) {
-            item = obj[keys[i]]
-            if (item && Object.prototype.toString.call(item).slice(8, -1).toLowerCase() === 'object') {
+            item = data[keys[i]]
+            if (Object.prototype.toString.call(item).slice(8, -1) === 'Object') {
                 result[keys[i]] = traverse(item)
             } else {
                 result[keys[i]] = item
             }
         }        
         return result    
+    }
+}
+
+
+/**
+ * @description JSON 深拷贝
+ * @function ven$deepClone2
+ * @param {object|array} json 被深拷贝的 JSON 数据
+ * @return {object|array}
+ */
+function ven$deepClone2(data) {
+    return traverse(data)
+
+    function traverse(data) {
+        if (
+            typeof data !== 'object'
+            || data === null
+            || data instanceof Date
+            || data instanceof ArrayBuffer
+            || data instanceof Uint8ClampedArray
+            || data instanceof Uint8Array
+            || data instanceof Uint16Array
+            || data instanceof Uint32Array
+        ) {
+            return data
+        }
+        if (Array.isArray(data)) {
+            return data.map(traverse)
+        }
+        const obj = {}
+        for (let key in data) {
+            if (data.hasOwnProperty(key)) {
+                obj[key] = traverse(data[key])
+            }
+        }
+        return obj
     }
 }
