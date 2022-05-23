ba2fc9ae21276d36584f3fe7c19b014ca3f8b4f4
ctime-zliang <1156282805@qq.com>
2022-05-23 11:25:34 +0800

    feta: modify - daily commit

--- webcase/$ademo/js/eventLoop-task.tests.js
+++ webcase/$ademo/js/eventLoop-task.tests.js
@@ -1,30 +1,62 @@
+// /* 
+//     https://zhuanlan.zhihu.com/p/142742003
+//  */
+
+
 // let time = {}
 // function taskTest() {
+//     console.log(`=======================`)
 //     window.setTimeout(() => {
-//         time.start = performance.now()
-//         console.log(`setTimeout 1`)
-//         // document.body.innerHTML = createHtmlString()
 //         window.requestAnimationFrame(() => {
-//             console.log(`rAF 1`)
+//             time.st1RAf1 = performance.now()
+//             console.log(`rAF 1 - setTimeout 1`)
 //         })
+//         window.requestAnimationFrame(() => {
+//             time.st1RAf2 = performance.now()
+//             console.log(`rAF 2 - setTimeout 1`)
+//         })
+//         console.log(`setTimeout 1`)
+//         time.st1 = performance.now()
+//         // ven$choke(100)
+//         // document.body.innerHTML = `<div>${Math.random()}</div>`
+//         document.body.innerHTML = createHtmlString()
 //     })
 //     window.setTimeout(() => {
-//         console.log(`setTimeout 2`, performance.now() - time.start)
 //         window.requestAnimationFrame(() => {
-//             console.log(`rAF 2`)
+//             time.st2RAf1 = performance.now()
+//             console.log(`rAF 1 - setTimeout 2`)
 //         })
+//         window.requestAnimationFrame(() => {
+//             time.st2RAf2 = performance.now()
+//             console.log(`rAF 2 - setTimeout 2`)
+//         })
+//         console.log(`setTimeout 2`)
+//         time.st2 = performance.now()
 //     })
 //     Promise.resolve().then(() => {
+//         time.rAf1 = performance.now()
 //         console.log(`promise 1`)
 //     })
-//     Promise.resolve().then(() => {
+//     Promise.resolve().then(() => {        
 //         console.log(`promise 2`)
 //     })
+//     window.requestAnimationFrame(() => {
+//         time.rAf1 = performance.now()
+//         console.log(`rAF 1`)
+//     })
+//     window.requestAnimationFrame(() => {
+//         time.rAf2 = performance.now()
+//         console.log(`rAF 2`)
+//     })
+
+//     window.setTimeout(() => {
+//         console.log(time)
+//     }, 750)
 // }
 
 // function createHtmlString() {
 //     let htmlString = ``
-//     for (let i = 0; i < 10000; i++) {
+//     for (let i = 0; i < 1000; i++) {
 //         htmlString += `<div>${i} - ${Math.random()}</div>`
 //     }
 //     return htmlString
