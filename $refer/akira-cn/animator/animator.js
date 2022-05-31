93c19c4daa3045344ba98318293079826bea833e
richardadda <1156282805@qq.com>
2022-06-01 00:18:55 +0800

    feat: modify - add demo

---
+++ $refer/akira-cn/animator/animator.js
@@ -0,0 +1,43 @@
+
+class Animator {
+  constructor(duration, update, easing) {
+    this.duration = duration
+    this.update = update
+    this.easing = easing
+  }
+  animate(startTime) {
+    startTime = startTime || 0
+
+    const duration = this.duration,
+      update = this.update,
+      easing = this.easing,
+      self = this
+
+    return new Promise(((resolve, reject) => {
+      let qId = 0
+      function step(timestamp) {
+        startTime = startTime || timestamp
+        const p = Math.min(1.0, (timestamp - startTime) / duration)
+
+        update.call(self, easing ? easing(p) : p, p)
+
+        if(p < 1.0) {
+          qId = requestAnimationFrame(step)
+        } else {
+          resolve(startTime + duration)
+        }
+      }
+
+      self.cancel = function () {
+        cancelAnimationFrame(qId)
+        update.call(self, 0, 0)
+        resolve(startTime + duration)
+      }
+      qId = requestAnimationFrame(step)
+    }))
+  }
+  ease(easing) {
+    return new Animator(this.duration, this.update, easing)
+  }
+}
+
