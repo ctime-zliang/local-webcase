98cfb3cb5dceed9661d34aaece76e1f30808c300
ctime-zliang <1156282805@qq.com>
2022-11-26 22:23:05 +0800

    feat: modify - daily commit

---
+++ $vendor/algorithm/LinkList/List.Base.js
@@ -0,0 +1,14 @@
+/**
+ * @description 链表节点
+ * @class ListNode
+ * @param {any} val 链表节点值
+ * @param {ListNode} next 下一个链表节点
+ * @return {undefined}
+ */
+class Ven$LinkListNodeBase {
+	constructor(val, next, prev) {
+		this.val = val === undefined ? 0 : val
+		this.next = next === undefined ? null : next
+		this.prev = prev === undefined ? null : prev
+	}
+}
