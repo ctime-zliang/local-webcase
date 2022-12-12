ba6a6c79ee200acc9723e51f9c127b317a37e6b8
ctime-zliang <1156282805@qq.com>
2022-12-12 23:38:47 +0800

    feat: modify - daily commit

--- $vendor/algorithm/LinkList/linkList2array.js
+++ $vendor/algorithm/LinkList/linkList2array.js
@@ -1,7 +1,7 @@
 /**
- * @description 将链表转换成数组
+ * @description 将单向链表转换成数组
  * @function ven$setSinglyLinkList2Array
- * @param {Ven$LinkListNode} list 链表
+ * @param {Ven$SinglyLinkListNode} list 单向链表
  * @return {array}
  */
 function ven$setSinglyLinkList2Array(list) {
@@ -15,18 +15,57 @@ function ven$setSinglyLinkList2Array(list) {
 }
 
 /**
- * @description 将数组转换成链表
+ * @description 将数组转换成单向链表
  * @function ven$setArray2SinglyLinkList
  * @param {array} arr 数组
- * @return {Ven$LinkListNode}
+ * @return {Ven$SinglyLinkListNode}
  */
 function ven$setArray2SinglyLinkList(arr) {
-	const head = new Ven$SinglyLinkListNode(arr[0], null)
+	const head = new Ven$SinglyLinkListNode(arr[0], undefined)
 	let p = head
 	for (let i = 1; i < arr.length; i++) {
-		const node = new Ven$SinglyLinkListNode(arr[i], null)
+		const node = new Ven$SinglyLinkListNode(arr[i], undefined)
 		p.next = node
 		p = node
 	}
 	return head
 }
+
+/**
+ * @description 将数组转换成循环链表
+ * @function ven$setArray2CyclicLinkList
+ * @param {array} arr 数组
+ * @return {Ven$SinglyLinkListNode}
+ */
+ function ven$setArray2CyclicLinkList(arr) {
+	const head = new Ven$SinglyLinkListNode(arr[0], undefined)
+	let p = head
+	for (let i = 1; i < arr.length; i++) {
+		const node = new Ven$SinglyLinkListNode(arr[i], undefined)
+		p.next = node
+		p = node
+	}
+	p.next = head
+	return head
+}
+
+/**
+ * @description 将数组转换成双向链表
+ * @function ven$setArray2DoublyLinkList
+ * @param {array} arr 数组
+ * @return {Ven$SinglyLinkListNode}
+ */
+ function ven$setArray2DoublyLinkList(arr) {
+	const head = new Ven$DoublyLinkListNode(arr[0], undefined, undefined)
+	let p = head
+	let t = null
+	for (let i = 1; i < arr.length; i++) {
+		const node = new Ven$DoublyLinkListNode(arr[i], undefined, undefined)
+		p.next = node
+		t = p
+		p = node
+		p.prev = t
+	}
+	return head
+}
+
---
+++ webcase/algorithm/LinkList/doublyLinkList/script/main.js
@@ -0,0 +1,8 @@
+const array = [3, 5, 8, 1, 9, 6]
+
+function main() {
+	const doublyLinkList = ven$setArray2DoublyLinkList(array)	
+	console.log(`Doubly Link List`, doublyLinkList)
+}
+
+main()
