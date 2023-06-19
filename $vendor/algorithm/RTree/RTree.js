6acb45d6c6123ba3f74bccee20350f099e3cafd4
ctime-zliang <1156282805@qq.com>
2023-06-19 19:27:15 +0800

    feat: modify - daily commit

--- $vendor/algorithm/RTree/RTree.js
+++ $vendor/algorithm/RTree/RTree.js
@@ -1,3 +1,70 @@
 class Ven$RTree {
-	constructor(w) {}
+	constructor(width) {
+		this.root = null
+		this.minWidth = 0
+		this.maxWidth = 0
+		this.inital(width)
+	}
+
+	inital(width) {
+		let minWidth = 3
+		let maxWidth = 6
+		if (!isNaN(width)) {
+			minWidth = Math.floor(width / 2.0)
+			maxWidth = width
+		}
+		let rootTree = {
+			sx: 0,
+			sy: 0,
+			w: 0,
+			h: 0,
+			id: 'root',
+			nodes: [],
+		}
+		this.root = rootTree
+		this.minWidth = minWidth
+		this.maxWidth = maxWidth
+	}
+
+	insertSubtree(node, root) {
+		Ven$Rtree_insertSubtree(node, root)
+	}
+
+	getTree() {
+		return this.rootTree
+	}
+
+	setTree(newTree, where) {
+		if (!where) {
+			where = rootTree
+		}
+		return Ven$Rtree_attachData(where, newTree)
+	}
+
+	search(rect, returnNode, returnArray) {
+		returnArray = returnArray || []
+		return Ven$Rtree_searchSubtree(rect, returnNode, returnArray, this.rootTree)
+	}
+
+	remove(rect, obj) {
+		if (!obj || typeof obj === 'function') {
+			return Ven$Rtree_removeArea(rect, obj)
+		} else {
+			return Ven$Rtree_removeObj(rect, obj)
+		}
+	}
+
+	insert(rect, obj) {
+		let retArray = Ven$Rtree_insertSubtree(
+			{
+				x: rect.x,
+				y: rect.y,
+				w: rect.w,
+				h: rect.h,
+				leaf: obj,
+			},
+			this.rootTree
+		)
+		return retArray
+	}
 }
--- $vendor/algorithm/RTree/utils.js
+++ $vendor/algorithm/RTree/utils.js
@@ -72,12 +72,12 @@ function Ven$Rtree_removeSubtree(rect, obj, root) {
 			}
 			retObj.nodes = []
 			if (hitStack.length === 0 && tree.nodes.length <= 1) {
-				retObj.nodes = searchSubtree(tree, true, retObj.nodes, tree)
+				retObj.nodes = Ven$Rtree_searchSubtree(tree, true, retObj.nodes, tree)
 				tree.nodes = []
 				hitStack.push(tree)
 				countStack.push(1)
 			} else if (hitStack.length > 0 && tree.nodes.length < minWidth) {
-				retObj.nodes = searchSubtree(tree, true, retObj.nodes, tree)
+				retObj.nodes = Ven$Rtree_searchSubtree(tree, true, retObj.nodes, tree)
 				tree.nodes = []
 			} else {
 				delete retObj.nodes
@@ -113,8 +113,8 @@ function Ven$Rtree_chooseLeafSubtree(rect, root) {
 				break
 			}
 			let oldLRatio = Ven$Rtree_Rectangle.squarifiedRatio(ltree.w, ltree.h, ltree.nodes.length + 1)
-			let nw = Math.max(ltree.x + ltree.w, rect.sx + rect.w) - Math.min(ltree.x, rect.sx)
-			let nh = Math.max(ltree.y + ltree.h, rect.sy + rect.h) - Math.min(ltree.y, rect.sy)
+			let nw = Math.max(ltree.sx + ltree.w, rect.sx + rect.w) - Math.min(ltree.sx, rect.sx)
+			let nh = Math.max(ltree.sy + ltree.h, rect.sy + rect.h) - Math.min(ltree.sy, rect.sy)
 			let lratio = Ven$Rtree_Rectangle.squarifiedRatio(nw, nh, ltree.nodes.length + 2)
 			if (bestChoiceIndex < 0 || Math.abs(lratio - oldLRatio) < bestChoiceArea) {
 				bestChoiceArea = Math.abs(lratio - oldLRatio)
@@ -126,9 +126,228 @@ function Ven$Rtree_chooseLeafSubtree(rect, root) {
 }
 
 function Ven$Rtree_linearSplit(nodes) {
-	let n = pickLinear(nodes)
+	const n = Ven$Rtree_pickLinear(nodes)
 	while (nodes.length > 0) {
-		pickNext(nodes, n[0], n[1])
+		Ven$Rtree_pickNext(nodes, n[0], n[1])
 	}
 	return n
+}
+
+function Ven$Rtree_pickNext(nodes, a, b) {
+	const areaA = Ven$Rtree_Rectangle.squarifiedRatio(a.w, a.h, a.nodes.length + 1)
+	const areaB = Ven$Rtree_Rectangle.squarifiedRatio(b.w, b.h, b.nodes.length + 1)
+	let highAreaDelta
+	let highAreaNode
+	let lowestGrowthGroup
+
+	for (let i = nodes.length - 1; i >= 0; i--) {
+		let l = nodes[i]
+		let newAreaA = {}
+		newAreaA.sx = Math.min(a.sx, l.sx)
+		newAreaA.sy = Math.min(a.sy, l.sy)
+		newAreaA.w = Math.max(a.sx + a.w, l.sx + l.w) - newAreaA.sx
+		newAreaA.h = Math.max(a.sy + a.h, l.sy + l.h) - newAreaA.sy
+		let changeNewAreaA = Math.abs(Ven$Rtree_Rectangle.squarifiedRatio(newAreaA.w, newAreaA.h, a.nodes.length + 2) - areaA)
+		/* ... */
+		let newAreaB = {}
+		newAreaB.sx = Math.min(b.sx, l.sx)
+		newAreaB.sy = Math.min(b.sy, l.sy)
+		newAreaB.w = Math.max(b.sx + b.w, l.sx + l.w) - newAreaB.sx
+		newAreaB.h = Math.max(b.sy + b.h, l.sy + l.h) - newAreaB.sy
+		let changeNewAreaB = Math.abs(Ven$Rtree_Rectangle.squarifiedRatio(newAreaB.w, newAreaB.h, b.nodes.length + 2) - areaB)
+		/* ... */
+		if (!highAreaNode || !highAreaDelta || Math.abs(changeNewAreaB - changeNewAreaA) < highAreaDelta) {
+			highAreaNode = i
+			highAreaDelta = Math.abs(changeNewAreaB - changeNewAreaA)
+			lowestGrowthGroup = changeNewAreaB < changeNewAreaA ? b : a
+		}
+	}
+	const tempNode = nodes.splice(highAreaNode, 1)[0]
+	if (a.nodes.length + nodes.length + 1 <= minWidth) {
+		a.nodes.push(tempNode)
+		Ven$Rtree_Rectangle.expandRectangle(a, tempNode)
+	} else if (b.nodes.length + nodes.length + 1 <= minWidth) {
+		b.nodes.push(tempNode)
+		Ven$Rtree_Rectangle.expandRectangle(b, tempNode)
+	} else {
+		lowestGrowthGroup.nodes.push(tempNode)
+		Ven$Rtree_Rectangle.expandRectangle(lowestGrowthGroup, tempNode)
+	}
+}
+
+function Ven$Rtree_pickLinear(nodes) {
+	let lowestHighX = nodes.length - 1
+	let highestLowX = 0
+	let lowestHighY = nodes.length - 1
+	let highestLowY = 0
+	let t1
+	let t2
+
+	for (let i = nodes.length - 2; i >= 0; i--) {
+		let l = nodes[i]
+		if (l.sx > nodes[highestLowX].sx) {
+			highestLowX = i
+		} else if (l.sx + l.w < nodes[lowestHighX].sx + nodes[lowestHighX].w) {
+			lowestHighX = i
+		}
+		if (l.sy > nodes[highestLowY].sy) {
+			highestLowY = i
+		} else if (l.sy + l.h < nodes[lowestHighY].sy + nodes[lowestHighY].h) {
+			lowestHighY = i
+		}
+	}
+	let dx = Math.abs(nodes[lowestHighX].sx + nodes[lowestHighX].w - nodes[highestLowX].sx)
+	let dy = Math.abs(nodes[lowestHighY].sy + nodes[lowestHighY].h - nodes[highestLowY].sy)
+	if (dx > dy) {
+		if (lowestHighX > highestLowX) {
+			t1 = nodes.splice(lowestHighX, 1)[0]
+			t2 = nodes.splice(highestLowX, 1)[0]
+		} else {
+			t2 = nodes.splice(highestLowX, 1)[0]
+			t1 = nodes.splice(lowestHighX, 1)[0]
+		}
+	} else {
+		if (lowestHighY > highestLowY) {
+			t1 = nodes.splice(lowestHighY, 1)[0]
+			t2 = nodes.splice(highestLowY, 1)[0]
+		} else {
+			t2 = nodes.splice(highestLowY, 1)[0]
+			t1 = nodes.splice(lowestHighY, 1)[0]
+		}
+	}
+	return [
+		{
+			sx: t1.sx,
+			sy: t1.sy,
+			w: t1.w,
+			h: t1.h,
+			nodes: [t1],
+		},
+		{
+			sx: t2.sx,
+			sy: t2.sy,
+			w: t2.w,
+			h: t2.h,
+			nodes: [t2],
+		},
+	]
+}
+
+function Ven$Rtree_attachData(node, moreTree) {
+	node.nodes = moreTree.nodes
+	node.sx = moreTree.sx
+	node.sy = moreTree.sy
+	node.w = moreTree.w
+	node.h = moreTree.h
+	return node
+}
+
+function Ven$Rtree_searchSubtree(rect, returnNode, returnArray, root) {
+	let hitStack = []
+	if (!Ven$Rtree_Rectangle.overlapRectangle(rect, root)) {
+		return returnArray
+	}
+	hitStack.push(root.nodes)
+	while (hitStack.length > 0) {
+		let nodes = hitStack.pop()
+		for (let i = nodes.length - 1; i >= 0; i--) {
+			let ltree = nodes[i]
+			if (Ven$Rtree_Rectangle.overlapRectangle(rect, ltree)) {
+				if (ltree.nodes) {
+					hitStack.push(ltree.nodes)
+				} else if (ltree.leaf) {
+					if (!returnNode) {
+						returnArray.push(ltree.leaf)
+					} else {
+						returnArray.push(ltree)
+					}
+				}
+			}
+		}
+	}
+	return returnArray
+}
+
+function Ven$Rtree_insertSubtree(node, root) {
+	let bc
+	if (root.nodes.length === 0) {
+		root.sx = node.sx
+		root.sy = node.sy
+		root.w = node.w
+		root.h = node.h
+		root.nodes.push(node)
+		return
+	}
+	let treeStack = Ven$Rtree_chooseLeafSubtree(node, root)
+	let retObj = node
+	let pbc
+	while (treeStack.length > 0) {
+		if (bc && bc.nodes && bc.nodes.length === 0) {
+			pbc = bc // Past bc
+			bc = treeStack.pop()
+			for (let t = 0; t < bc.nodes.length; t++) {
+				if (bc.nodes[t] === pbc || bc.nodes[t].nodes.length === 0) {
+					bc.nodes.splice(t, 1)
+					break
+				}
+			}
+		} else {
+			bc = treeStack.pop()
+		}
+		if (retObj.leaf || retObj.nodes || Array.isArray(retObj)) {
+			if (Array.isArray(retObj)) {
+				for (let ai = 0; ai < retObj.length; ai++) {
+					Ven$Rtree_Rectangle.expandRectangle(bc, retObj[ai])
+				}
+				bc.nodes = bc.nodes.concat(retObj)
+			} else {
+				Ven$Rtree_Rectangle.expandRectangle(bc, retObj)
+				bc.nodes.push(retObj)
+			}
+			if (bc.nodes.length <= maxWidth) {
+				retObj = {
+					sx: bc.sx,
+					sy: bc.sy,
+					w: bc.w,
+					h: bc.h,
+				}
+			} else {
+				let a = linearSplit(bc.nodes)
+				retObj = a
+				if (treeStack.length < 1) {
+					bc.nodes.push(a[0])
+					treeStack.push(bc)
+					retObj = a[1]
+				}
+				/*else {
+                    delete bc;
+                }*/
+			}
+		} else {
+			Ven$Rtree_Rectangle.expandRectangle(bc, retObj)
+			retObj = {
+				sx: bc.sx,
+				sy: bc.sy,
+				w: bc.w,
+				h: bc.h,
+			}
+		}
+	}
+}
+
+function Ven$Rtree_removeArea(rect) {
+	let numberDeleted = 1
+	let retArray = []
+	let deleted
+	while (numberDeleted > 0) {
+		deleted = Ven$Rtree_removeSubtree(rect, false, rootTree)
+		numberDeleted = deleted.length
+		retArray = retArray.concat(deleted)
+	}
+	return retArray
+}
+
+function Ven$Rtree_removeObj(rect, obj) {
+	let retArray = Ven$Rtree_removeSubtree(rect, obj, rootTree)
+	return retArray
 }
