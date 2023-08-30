3f3fc093789a2d66cbb96de9afad1ad4f13b4dd9
ctime-zliang <1156282805@qq.com>
2023-08-30 11:09:17 +0800

    feat: modify - daily commit

--- $vendor/algorithm/RTree/utils.js
+++ $vendor/algorithm/RTree/utils.js
@@ -364,6 +364,18 @@ function Ven$Rtree_searchSubtree(rect, root, isGetNodeDataOnly = true) {
 	if (!Ven$Rtree_Rectangle.overlapRectangle(rect, root)) {
 		return result
 	}
+	/**
+	 * hitStack 是一个元素类型为数组的数组
+	 * 在任意一次 for 遍历过程中, 在满足条件的情况下, 某一节点的所有子节点将作为一个整体, 存入 hitStack 中
+	 *
+	 * 由于
+	 * 		1. hitStack.pop()
+	 * 		2. for 倒序遍历
+	 * 遍历某一节点的子节点列表时, 都会从最左子节点开始
+	 * 当某一个子节点 C(n) 满足条件时, 将 C(n) 的子节点列表作为一个整体存入 hitStack 中
+	 * 当某一个子节点 C(n) 不满足条件时, 继续遍历 C(n) 的前一个兄弟节点
+	 * 整个遍历过程将是一个类似"深度优先"的处理流程
+	 */
 	const hitStack = [root.nodes]
 	while (hitStack.length > 0) {
 		const nodes = hitStack.pop()
