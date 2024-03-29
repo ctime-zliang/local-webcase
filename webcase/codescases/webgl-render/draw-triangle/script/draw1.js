8d8d887001c42c73e3b5b692082dc7ed8e262e81
ctime-zliang <1156282805@qq.com>
2024-03-29 11:18:22 +0800

    feat: modify - daily commit

---
+++ webcase/codescases/webgl-render/draw-triangle/script/draw1.js
@@ -0,0 +1,43 @@
+function drawCanvas1(containerElement) {
+	const VS = `
+		precision mediump float;
+		attribute vec2 a_Position;
+		void main() {
+			gl_Position = vec4(a_Position, 0, 1);
+			gl_PointSize = 10.0;
+		}
+	`
+
+	const FS = `
+		precision mediump float;
+		uniform vec4 u_Color;
+		void main() {
+			vec4 color = u_Color / vec4(255, 255, 255, 1.0);
+			gl_FragColor = color;
+		}
+	`
+
+	const positions = [1, 0, 0, 1, 0, 0]
+
+	const canvasElement = containerElement.querySelector('canvas')
+	const gl = initWebGLContext(canvasElement)
+
+	const vertexShader = createShader(gl, gl.VERTEX_SHADER, VS)
+	const fragmentShader = createShader(gl, gl.FRAGMENT_SHADER, FS)
+	const program = createProgram(gl, vertexShader, fragmentShader)
+
+	const typeArray = new Float32Array(positions)
+	const buffer = createBuffer(gl, typeArray)
+
+	gl.useProgram(program)
+
+	gl.clearColor(0.0, 0.0, 0.0, 1.0)
+	gl.clear(gl.COLOR_BUFFER_BIT)
+
+	const a_Position = gl.getAttribLocation(program, 'a_Position')
+
+	gl.enableVertexAttribArray(a_Position)
+	gl.vertexAttribPointer(a_Position, 2, gl.FLOAT, false, 0, 0)
+
+	gl.drawArrays(gl.TRIANGLES, 0, 3)
+}
