e57163740bd0314c820c8dee397809816221467c
ctime-zliang <1156282805@qq.com>
2024-04-26 11:49:35 +0800

    feat: modify - daily commit

--- webcase/codescases/webgl-render/common/utils/utils.js
+++ webcase/codescases/webgl-render/common/utils/utils.js
@@ -1,14 +1,33 @@
-function loadTexture(gl, src, attribute, callback) {
-	const img = new Image()
-	img.crossOrigin = 'anonymous'
-	img.onload = function () {
+function loadTexture(gl, src, uniform, callback) {
+	const initTexture = (gl, imageInstance) => {
+		/**
+		 * 激活纹理单元
+		 * 		0 号纹理单元
+		 */
 		gl.activeTexture(gl.TEXTURE0)
+		/**
+		 * 创建并绑定纹理对象
+		 */
 		const texture = gl.createTexture()
 		gl.bindTexture(gl.TEXTURE_2D, texture)
-		gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, this)
+		/**
+		 * 纹理参数设置
+		 */
 		gl.texParameterf(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR)
 		gl.texParameterf(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR)
-		gl.uniform1i(attribute, 0)
+		/**
+		 * 使用 Image 对象填充纹理内容
+		 */
+		gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, imageInstance)
+	}
+	const img = new Image()
+	img.crossOrigin = 'anonymous'
+	img.onload = function (e) {
+		initTexture(gl, this)
+		/**
+		 * 由于被激活且已绑定了纹理对象的纹理单元为 0 号纹理单元, 因此此处将对应纹理单元的编号(即 0)传递给着色器中的采样器变量
+		 */
+		gl.uniform1i(uniform, 0)
 		callback && callback()
 	}
 	img.src = src
--- webcase/codescases/webgl-render/draw-image-texture/script/draw1.js
+++ webcase/codescases/webgl-render/draw-image-texture/script/draw1.js
@@ -9,31 +9,33 @@ function drawCanvas1(containerElement) {
 		attribute vec2 a_Position;
 		// 接收画布尺寸 (width, height)
 		attribute vec2 a_CanvasSize;
-		// 接收纹理坐标
+		// 纹理坐标
 		attribute vec2 a_Uv;
-		// 传递给片元着色器的纹理坐标
 		varying vec2 v_Uv;
 		void main() {
 			vec2 position = (a_Position / a_CanvasSize) * 2.0 - 1.0; 
 			position = position * vec2(1.0, -1.0);
 			gl_Position = vec4(position, 0, 1);
-			gl_PointSize = 5.0;
 			v_Uv = a_Uv;
 		}
 	`
 	const FS = `
 		// 设置浮点数精度为中等精度
 		precision mediump float;
-		// 接收顶点着色器传递的纹理坐标
+		// 接收纹理坐标
 		varying vec2 v_Uv;
-		// 接收纹理数据
-		uniform sampler2D u_Texture;
+		// 接收纹理数据(内容)
+		uniform sampler2D u_Sampler;
 		void main() {
-			gl_FragColor = texture2D(u_Texture, v_Uv);
+			gl_FragColor = texture2D(u_Sampler, v_Uv);
 		}
 	`
 
-	const datas = [30, 30, 0, 0, 30, 300, 0, 1, 300, 300, 1, 1, 30, 30, 0, 0, 300, 300, 1, 1, 300, 30, 1, 0]
+	const datas = [
+		/* ... */
+		30, 30, 0, 0 /* ... */, 30, 300, 0, 1 /* ... */, 300, 300, 1, 1 /* ... */, 30, 30, 0, 0 /* ... */, 300, 300, 1, 1 /* ... */, 300, 30, 1, 0,
+		/* ... */
+	]
 
 	const canvasElement = containerElement.querySelector('canvas')
 	const gl = initWebGLContext(canvasElement)
@@ -50,7 +52,7 @@ function drawCanvas1(containerElement) {
 	const a_Position = gl.getAttribLocation(program, 'a_Position')
 	const a_CanvasSize = gl.getAttribLocation(program, 'a_CanvasSize')
 	const a_Uv = gl.getAttribLocation(program, 'a_Uv')
-	const u_Texture = gl.getUniformLocation(program, 'u_Texture')
+	const u_Sampler = gl.getUniformLocation(program, 'u_Sampler')
 
 	gl.enableVertexAttribArray(a_Position)
 	gl.enableVertexAttribArray(a_Uv)
@@ -68,10 +70,7 @@ function drawCanvas1(containerElement) {
 	gl.bindBuffer(gl.ARRAY_BUFFER, datasBuffer)
 	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(datas), gl.STATIC_DRAW)
 
-	/**
-	 * 图片尺寸必须满足 2^n
-	 */
-	loadTexture(gl, '../common/images/demo-1024x1024.jpg', u_Texture, () => {
+	loadTexture(gl, '../common/images/demo-1024x1024.jpg', u_Sampler, () => {
 		gl.drawArrays(gl.TRIANGLES, 0, datas.length / 4)
 	})
 }
